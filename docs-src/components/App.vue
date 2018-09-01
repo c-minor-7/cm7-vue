7e74906e0878b95e853b07eba4392fc986ecc8c7
Jason Yu <me@ycmjason.com>
2018-09-01 17:41:15 +0100

    feat: added cm7s sources

--- docs-src/components/App.vue
+++ docs-src/components/App.vue
@@ -5,81 +5,36 @@
       :class="{ cm7Error: cm7Error }"
       v-model="src"></textarea>
     <Cm7
-      class="cm7Container"
+      class="cm7"
       :src="src"
       @error="cm7Error = $event" />
   </div>
 </template>
 
 <script>
+import { readFileSync } from 'fs';
 import Cm7 from '../../src/Cm7.vue';
+
+const songs = {
+  'here-we-are': readFileSync(__dirname + '/../cm7s/here-we-are.cm7', 'utf8'),
+  'jimjim': readFileSync(__dirname + '/../cm7s/jimjim.cm7', 'utf8'),
+};
 
 export default {
   components: { Cm7 },
   data: () => ({
     cm7Error: false,
-    src: `key = D
-
-:: intro ::
-1 4
--
-1 4
--
-
-:: verse ::
-1 4
-() Here we are in the (future) now
-1 4 2m
-() Here we are in the (place) that we wanted to (be)
-7b
-To (be)
-
-1 4
-() Everything in your (fantasy)
-1 4 2m
-() Everything has be(come) your reali(ty)
-7b
-(Hmmm)
-
-:: pre-chorus ::
-4 5
-() What a fool I (used) to be
-4 5
-() Should have known you mean the (most) to me
-4   4
-() Now I finally (see) it
-2m
-Now I finally (know) it
--
-All I really want
-2m
-() All I really want is
-
-
-:: chorus ::
-1
-(I) just wanna be with you
-1
-(I) just wanna be with you
-       4
-Right (now)
-1
-(I) just wanna be with you
-1
-(I) just wanna be with you
-4
-Right (now)
-2
-I (didn't) know
-2m
-It (runs) so fast
-2m7b5
-And I (can't) catch up`
+    songs,
+    src: songs['here-we-are'],
   }),
 };
 </script>
 
 <style scoped>
+* {
+  box-sizing: border-box;
+}
+
 #app {
   display: flex;
   max-width: 1200px;
@@ -91,8 +46,9 @@ textarea {
   width: 50%;
 }
 
-.cm7Container {
+.cm7 {
   width: 50%;
+  padding: 1rem;
 }
 
 .cm7Src {
